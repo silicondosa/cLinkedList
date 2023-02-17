58f5881978f344ca6753a9fd5ac6988d85999d92
Suraj Chakravarthi <cluster.root@gmail.com>
2023-02-16 16:32:49 -0800

    Commented the cLinkedList header and generated doxygen documentation.

---
+++ docs/index.js
@@ -0,0 +1,13 @@
+var index =
+[
+    [ "Supported platforms and binaries", "index.html#autotoc_md1", null ],
+    [ "Usage", "index.html#autotoc_md2", null ],
+    [ "Supported functions", "index.html#autotoc_md3", [
+      [ "1. Linked list creation functions", "index.html#autotoc_md4", null ],
+      [ "2. Linked list reading functions", "index.html#autotoc_md5", null ],
+      [ "3. Linked list status functions", "index.html#autotoc_md6", null ],
+      [ "4. Linked list unlinking functions", "index.html#autotoc_md7", null ]
+    ] ],
+    [ "Documentation", "index.html#autotoc_md8", null ],
+    [ "Licensing", "index.html#autotoc_md9", null ]
+];
